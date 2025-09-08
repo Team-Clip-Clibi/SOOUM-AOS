06f548c50cb60893c9489a71d66c45a818591b09
SUNGIL KIM <sonny0204@partron.co.kr>
2025-09-08 17:17:09 +0900

    텍스트 컴포넌트 지정 완료

--- core/core-design/src/main/java/com/phew/core_design/TextComponent.kt
+++ core/core-design/src/main/java/com/phew/core_design/TextComponent.kt
@@ -38,6 +38,87 @@ object TextComponent {
             lineHeight = 39.2.sp,
             fontFamily = FontFamily(Font(R.font.bold)),
             fontWeight = FontWeight(700),
-            color = Color(0xFF212121)
+            color = NeutralColor.BLACK
+        )
+
+    val HEAD_2_B_24
+        @Composable get() = fixFontSize(
+            fontSize = 24.sp,
+            lineHeight = 33.6.sp,
+            fontFamily = FontFamily(Font(R.font.bold)),
+            fontWeight = FontWeight(700),
+            color = NeutralColor.BLACK
+        )
+    val HEAD_3_B_20
+        @Composable get() = fixFontSize(
+            fontSize = 20.sp,
+            lineHeight = 28.sp,
+            fontFamily = FontFamily(Font(R.font.bold)),
+            fontWeight = FontWeight(700),
+            color = NeutralColor.BLACK
+        )
+    val TITLE_1_SB_18
+        @Composable get() = fixFontSize(
+            fontSize = 18.sp,
+            lineHeight = 27.sp,
+            fontFamily = FontFamily(Font(R.font.semi_bold)),
+            fontWeight = FontWeight(600),
+            color = NeutralColor.BLACK
+        )
+    val TITLE_2_SB_16
+        @Composable get() = fixFontSize(
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            fontFamily = FontFamily(Font(R.font.semi_bold)),
+            fontWeight = FontWeight(600),
+            color = NeutralColor.BLACK
+        )
+    val SUBTITLE_1_M_16
+        @Composable get() = fixFontSize(
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            fontFamily = FontFamily(Font(R.font.medium)),
+            fontWeight = FontWeight(500),
+            color = NeutralColor.BLACK
+        )
+    val SUBTITLE_2_SB_14
+        @Composable get() = fixFontSize(
+            fontSize = 14.sp,
+            lineHeight = 21.sp,
+            fontFamily = FontFamily(Font(R.font.semi_bold)),
+            fontWeight = FontWeight(500),
+            color = NeutralColor.BLACK
+        )
+    val BODY_1_M_14
+        @Composable get() = fixFontSize(
+            fontSize = 14.sp,
+            lineHeight = 21.sp,
+            fontFamily = FontFamily(Font(R.font.medium)),
+            fontWeight = FontWeight(500),
+            color = NeutralColor.BLACK
+        )
+    val CAPTION_1_SB_12
+        @Composable get() = fixFontSize(
+            fontSize = 12.sp,
+            lineHeight = 18.sp,
+            fontFamily = FontFamily(Font(R.font.semi_bold)),
+            fontWeight = FontWeight(500),
+            color = NeutralColor.BLACK
+        )
+    val CAPTION_2_M_12
+        @Composable get() = fixFontSize(
+            fontSize = 12.sp,
+            lineHeight = 18.sp,
+            fontFamily = FontFamily(Font(R.font.medium)),
+            fontWeight = FontWeight(500),
+            color = NeutralColor.BLACK
+        )
+    val CAPTION_3_M_10
+        @Composable get() = fixFontSize(
+            fontSize = 10.sp,
+            lineHeight = 15.sp,
+            fontFamily = FontFamily(Font(R.font.medium)),
+            fontWeight = FontWeight(500),
+            color = NeutralColor.BLACK
         )
 }
