b72ec5a58a7aa4eb9a81c851fea60a609f8e87b6
SUNGIL KIM <sonny0204@partron.co.kr>
2025-09-08 16:07:55 +0900

    색 컴포넌트 지정 완료

---
+++ core/core-design/src/main/java/com/phew/core_design/ColorComponent.kt
@@ -0,0 +1,36 @@
+package com.phew.core_design
+
+import androidx.compose.ui.graphics.Color
+
+object NeutralColor {
+    val WHITE = Color(0xFFFFFFFF)
+    val GRAY_100 = Color(0xFFF5F7FA)
+    val GRAY_200 = Color(0xFFE4EAF1)
+    val GRAY_300 = Color(0xFFBFC9D3)
+    val GRAY_400 = Color(0xFF919DA9)
+    val GRAY_500 = Color(0xFF5D6369)
+    val GRAY_600 = Color(0xFF3A3F44)
+    val BLACK = Color(0xFF212121)
+}
+
+object Primary {
+    val LIGHT_1 = Color(0xFFD7F1F9)
+    val LIGHT_2 = Color(0xFF8CE1F4)
+    val MAIN = Color(0xFF20C6EC)
+    val DARK = Color(0xFF07ABD0)
+}
+
+object Success {
+    val L_GREEN = Color(0xFFD3F5EB)
+    val M_GREEN = Color(0xFF009262)
+}
+
+object Warning {
+    val L_YELLOW = Color(0xFFFFF0D7)
+    val M_YELLOW = Color(0xFFFFB240)
+}
+
+object Danger {
+    val L_RED = Color(0xFFFFE1DF)
+    val M_RED = Color(0xFFEE3A26)
+}
