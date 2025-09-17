c6c7ad44687d46ead7ff4e9fc8a43ec750fb6c09
SUNGIL KIM <sonny0204@partron.co.kr>
2025-09-17 09:38:37 +0900

    server api error code 정의

--- core/core-common/src/main/java/com/phew/core_common/_define.kt
+++ core/core-common/src/main/java/com/phew/core_common/_define.kt
@@ -1,6 +1,6 @@
 package com.phew.core_common
 
-
+//view nav
 const val NAV_SPLASH = "splash"
 const val NAV_ON_BOARDING = "onBoarding"
 const val NAV_SIGN_UP_AUTH_CODE = "authCode"
@@ -8,5 +8,9 @@ const val NAV_SIGN_UP_AGREEMENT = "agreement"
 const val NAV_SIGN_UP_NICKNAME = "nickName"
 const val NAV_SIGN_UP_PROFILE = "profile"
 const val NAV_SIGN_UP_FINISH = "finish"
-
+//domain error code
 const val ERROR_FAIL_JOB = "Fail job"
+//server api error code
+const val HTTP_NO_MORE_CONTENT = 204
+const val HTTP_INVALID_TOKEN = 401
+const val HTTP_NOT_FOUND = 404
