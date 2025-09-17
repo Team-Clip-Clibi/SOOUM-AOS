2847a769242f8160c751cbfb5c57b7b8d0509851
SUNGIL KIM <sonny0204@partron.co.kr>
2025-09-17 09:40:15 +0900

    data Result 정의

---
+++ core/core-common/src/main/java/com/phew/core_common/_dataResult.kt
@@ -0,0 +1,10 @@
+package com.phew.core_common
+
+sealed class DataResult<out T> {
+    data class Success<out T>(val data: T) : DataResult<T>()
+    data class Fail(
+        val code: Int = 0,
+        val message: String? = null,
+        val throwable: Throwable,
+    ) : DataResult<Nothing>()
+}
