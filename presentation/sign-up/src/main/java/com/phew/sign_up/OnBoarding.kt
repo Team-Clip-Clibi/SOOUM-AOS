afa48d6c1d1ffd5c2e5f74759f0db499d3e74e91
sonny8569 <sonny132@naver.com>
2025-09-20 01:19:42 +0900

    이미지 사이즈 조정

--- presentation/sign-up/src/main/java/com/phew/sign_up/OnBoarding.kt
+++ presentation/sign-up/src/main/java/com/phew/sign_up/OnBoarding.kt
@@ -195,9 +195,9 @@ private fun ExplainView(data: String) {
         Image(
             painter = painterResource(com.phew.core_design.R.drawable.ic_check_filled_blue),
             contentDescription = "data",
+            contentScale = ContentScale.Fit,
             modifier = Modifier
                 .size(24.dp)
-                .padding(1.25.dp)
         )
         Text(
             text = data,
@@ -217,13 +217,13 @@ private fun BottomView(
             .fillMaxWidth()
             .background(color = NeutralColor.WHITE)
             .navigationBarsPadding()
-            .padding(start = 16.dp, end = 16.dp)
+            .padding(start = 16.dp, end = 16.dp , bottom = 12.dp)
     ) {
         LargeButton.NoIconPrimary(
             buttonText = stringResource(R.string.onBoarding_btn_start),
             onClick = onClickStart
         )
-        Spacer(modifier = Modifier.height(8.dp))
+
         Text(
             text = stringResource(R.string.onBoarding_btn_already_sign_up),
             style = TextComponent.BODY_1_M_14,
@@ -232,6 +232,7 @@ private fun BottomView(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(32.dp)
+                .padding(top = 8.dp)
                 .clickable { onClickAlreadySignUp() }
         )
     }
