9df67d9feba79369d5e49b55d4cf2286983b2b4a
sonny8569 <sonny132@naver.com>
2025-09-28 20:01:44 +0900

    @OptIn(kotlinx.serialization.InternalSerializationApi::class) 추가

--- data/network/src/main/java/com/phew/network/dto/InfoDTO.kt
+++ data/network/src/main/java/com/phew/network/dto/InfoDTO.kt
@@ -2,6 +2,7 @@ package com.phew.network.dto
 
 import kotlinx.serialization.Serializable
 
+@OptIn(kotlinx.serialization.InternalSerializationApi::class)
 @Serializable
 data class InfoDTO(
     val encryptedDeviceId: String,
--- data/network/src/main/java/com/phew/network/dto/NoticeDto.kt
+++ data/network/src/main/java/com/phew/network/dto/NoticeDto.kt
@@ -2,13 +2,15 @@ package com.phew.network.dto
 
 import kotlinx.serialization.Serializable
 
+@OptIn(kotlinx.serialization.InternalSerializationApi::class)
 @Serializable
 data class NoticeDto(
     val notices: List<NoticeData>,
 )
 
+@OptIn(kotlinx.serialization.InternalSerializationApi::class)
 @Serializable
-data class NoticeData(
+class NoticeData(
     val title: String,
     val url: String,
     val createdAt: String,
