22004f70b681440adae4b6f5d594cb19dc05c6f1
SUNGIL KIM <sonny0204@partron.co.kr>
2025-09-30 10:26:44 +0900

    오타 수정

--- data/repository/src/main/java/com/phew/repository/mapper/Mapper.kt
+++ data/repository/src/main/java/com/phew/repository/mapper/Mapper.kt
@@ -71,6 +71,6 @@ internal fun NotificationDTO.toDomain(): Notification {
             )
         }
 
-        else -> throw IllegalArgumentException("Unknow type")
+        else -> throw IllegalArgumentException("Unknown type")
     }
 }
