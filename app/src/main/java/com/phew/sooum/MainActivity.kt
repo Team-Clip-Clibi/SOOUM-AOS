015d8c0e6e344da827d8ccb0084ee021cd8a3f19
SUNGIL KIM <sonny0204@partron.co.kr>
2025-10-02 09:04:39 +0900

    취소 선택  시 dialog 종료

--- app/src/main/java/com/phew/sooum/MainActivity.kt
+++ app/src/main/java/com/phew/sooum/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : ComponentActivity() {
                 },
                 feedLocationDialogNotShow = isLocationPermissionGranted,
                 closeDialog = {
-                    isLocationPermissionGranted = false
+                    isLocationPermissionGranted = true
                 }
             )
         }
