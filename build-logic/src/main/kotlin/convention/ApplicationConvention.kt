243c62b6c1748dc0c993f9fdc8efa8524ef5d59b
SUNGIL KIM <sonny0204@partron.co.kr>
2025-10-28 10:00:47 +0900

    1.0.5 버전 배포

--- build-logic/src/main/kotlin/convention/ApplicationConvention.kt
+++ build-logic/src/main/kotlin/convention/ApplicationConvention.kt
@@ -28,7 +28,7 @@ class ApplicationConvention : Plugin<Project> {
                 minSdk = 31
                 targetSdk = 36
                 versionCode = 1
-                versionName = "1.0.4"
+                versionName = "1.0.5"
                 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
             }
             buildTypes.getByName("release").apply {
