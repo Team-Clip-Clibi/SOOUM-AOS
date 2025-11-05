c2b52059787fe8293840ea37feb48098b611d4e0
sonny8569 <sonny132@naver.com>
2025-11-05 22:55:11 +0900

    paging 서버 응답 수정

--- data/network/src/main/java/com/phew/network/dto/response/card/ProfileCardDTO.kt
+++ data/network/src/main/java/com/phew/network/dto/response/card/ProfileCardDTO.kt
@@ -1,12 +1,14 @@
 package com.phew.network.dto.response.card
 
 import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ProfileCardDTO(
     @SerializedName("cardContents")
     val cardContents: List<CardContentDto>,
 )
-
+@Serializable
 data class CardContentDto(
     val cardId: Long,
     val cardImgName: String?,
