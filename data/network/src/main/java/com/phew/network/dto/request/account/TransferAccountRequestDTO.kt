1a688a7c2a297b33bf114138bb96e441d0a6bceb
SUNGIL KIM <sonny0204@partron.co.kr>
2025-11-25 16:49:15 +0900

    Serializable  - 제미나이 리뷰

--- data/network/src/main/java/com/phew/network/dto/request/account/TransferAccountRequestDTO.kt
+++ data/network/src/main/java/com/phew/network/dto/request/account/TransferAccountRequestDTO.kt
@@ -1,18 +1,12 @@
 package com.phew.network.dto.request.account
 
-import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class TransferAccountRequestDTO(
-    @SerializedName("transferCode")
     val transferCode: String,
-    @SerializedName("encryptedDeviceId")
     val encryptedDeviceId: String,
-    @SerializedName("deviceType")
     val deviceType: String,
-    @SerializedName("deviceModel")
     val deviceModel: String,
-    @SerializedName("deviceOsVersion")
     val deviceOsVersion: String
 )
