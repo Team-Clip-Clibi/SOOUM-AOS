ce01f83f93fa34b2e7afcdf15948d04bbc8efb3e
SUNGIL KIM <sonny0204@partron.co.kr>
2025-11-25 16:46:30 +0900

    data class equals 제거  - 제미나이 리뷰

--- data/device/datastore_local/src/main/java/com/phew/datastore_local/dto/ProfileInfoDTO.kt
+++ data/device/datastore_local/src/main/java/com/phew/datastore_local/dto/ProfileInfoDTO.kt
@@ -4,16 +4,4 @@ data class ProfileInfoDTO(
     val nickName: String,
     val profileImageUrl: String,
     val profileImageName: String,
-) {
-    override fun equals(other: Any?): Boolean {
-        if (other !is ProfileInfoDTO) return false
-        if (other.nickName != nickName) return false
-        if (other.profileImageUrl != profileImageUrl) return false
-        if (other.profileImageName != profileImageName) return false
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return javaClass.hashCode()
-    }
-}
+)
