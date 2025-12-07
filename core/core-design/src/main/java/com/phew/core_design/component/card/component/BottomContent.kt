e5f2ca8c0510f450350e0108ad83621c68549597
camai <jotolo@naver.com>
2025-12-07 17:37:44 +0900

    refactor: 로그 추가
    로그 추가

--- core/core-design/src/main/java/com/phew/core_design/component/card/component/BottomContent.kt
+++ core/core-design/src/main/java/com/phew/core_design/component/card/component/BottomContent.kt
@@ -115,9 +115,8 @@ internal fun LocationAndWriteTimeLabel(
             }
         }
         if (!writeTime.isNullOrEmpty()) {
-            val relativeTime = remember(writeTime) {
-                TimeUtils.getRelativeTimeString(writeTime)
-            }
+            val relativeTime = TimeUtils.getRelativeTimeString(writeTime)
+            SooumLog.d(TAG, "LocationAndWriteTimeLabel writeTime=$writeTime -> relative=$relativeTime")
             Text(
                 text = relativeTime,
                 style = TextComponent.CAPTION_2_M_12.copy(color = NeutralColor.GRAY_500),
