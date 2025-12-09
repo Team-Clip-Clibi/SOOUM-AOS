8ed2c28637ac6e04af7274aeeec22abc052e811f
sonny8569 <sonny132@naver.com>
2025-12-09 21:50:20 +0900

    1.0.12 배포

--- build-logic/src/main/kotlin/convention/ApplicationConvention.kt
+++ build-logic/src/main/kotlin/convention/ApplicationConvention.kt
@@ -28,7 +28,7 @@ class ApplicationConvention : Plugin<Project> {
                 minSdk = 31
                 targetSdk = 36
                 versionCode = 1
-                versionName = "1.0.11"
+                versionName = "1.0.12"
                 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
             }
             signingConfigs {
