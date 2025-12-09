837ee524a952f9734b5659d4341fcf4b755ee5c2
SUNGIL KIM <sonny0204@partron.co.kr>
2025-12-09 13:02:51 +0900

    Clarity 추가

---
+++ app/src/main/java/com/phew/sooum/clarity/ClarityInitializer.kt
@@ -0,0 +1,24 @@
+package com.phew.sooum.clarity
+
+import android.app.Application
+import android.util.Log
+import com.microsoft.clarity.Clarity
+import com.microsoft.clarity.ClarityConfig
+import com.phew.sooum.BuildConfig
+import javax.inject.Inject
+
+class ClarityInitializer @Inject constructor(
+    private val application: Application,
+    private val config: ClarityConfig,
+) {
+    fun init() {
+        if (BuildConfig.DEBUG) {
+            Log.e(this.javaClass.packageName, "Clarity is not support debug mode")
+        }
+        if (BuildConfig.CLARITY_PROJECT_ID.trim().isEmpty()) {
+            Log.e(this.javaClass.packageName, "Error Clarity id is null")
+            return
+        }
+        Clarity.initialize(application, config)
+    }
+}
