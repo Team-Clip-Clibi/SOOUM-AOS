a811ec27ab1066d138cb8cf11c421992c704be15
SUNGIL KIM <sonny0204@partron.co.kr>
2025-12-16 10:35:03 +0900

    알람 - 시간 출력 오류 수정

--- app/src/main/java/com/phew/sooum/ui/SooumApp.kt
+++ app/src/main/java/com/phew/sooum/ui/SooumApp.kt
@@ -1,12 +1,7 @@
 package com.phew.sooum.ui
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.only
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
