fc61401d4fcd5aa8e826ca2bc58ab2bcca4be424
sonny8569 <sonny132@naver.com>
2026-01-01 22:25:25 +0900

    version code 3 배포

--- build-logic/src/main/kotlin/convention/ApplicationConvention.kt
+++ build-logic/src/main/kotlin/convention/ApplicationConvention.kt
@@ -33,7 +33,7 @@ class ApplicationConvention : Plugin<Project> {
                 applicationId = "com.phew.sooum"
                 minSdk = 31
                 targetSdk = 36
-                versionCode = 2
+                versionCode = 3
                 versionName = "1.0.0"
                 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
             }
