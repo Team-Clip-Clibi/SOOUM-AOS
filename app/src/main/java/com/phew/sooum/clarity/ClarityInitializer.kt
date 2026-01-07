8ee330e6ce13fecbe6cd9ed29875d208488e5a81
sonny8569 <sonny132@naver.com>
2026-01-07 22:36:12 +0900

    클래리티 디버그 모드일시 데이터 방지

--- app/src/main/java/com/phew/sooum/clarity/ClarityInitializer.kt
+++ app/src/main/java/com/phew/sooum/clarity/ClarityInitializer.kt
@@ -1,9 +1,9 @@
 package com.phew.sooum.clarity
 
 import android.app.Application
-import android.util.Log
 import com.microsoft.clarity.Clarity
 import com.microsoft.clarity.ClarityConfig
+import com.phew.core_common.log.SooumLog
 import com.phew.sooum.BuildConfig
 import javax.inject.Inject
 
@@ -12,6 +12,10 @@ class ClarityInitializer @Inject constructor(
     private val config: ClarityConfig,
 ) {
     fun init() {
+        if (BuildConfig.DEBUG) {
+            SooumLog.d(msg = "Clarity not support DEBUG mode")
+            return
+        }
         Clarity.initialize(application, config)
     }
 }
