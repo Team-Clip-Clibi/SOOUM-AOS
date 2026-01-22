15cd12f780df2b4b05d873ebb43b551491fdad49
SUNGIL KIM <sonny0204@partron.co.kr>
2026-01-22 09:45:30 +0900

    내 프로필 검사 로직 수정

--- domain/src/main/java/com/phew/domain/usecase/CheckIsMyProfile.kt
+++ domain/src/main/java/com/phew/domain/usecase/CheckIsMyProfile.kt
@@ -1,20 +1,29 @@
 package com.phew.domain.usecase
 
+import com.phew.core_common.DataResult
 import com.phew.core_common.DomainResult
 import com.phew.core_common.ERROR_FAIL_JOB
-import com.phew.domain.BuildConfig
-import com.phew.domain.repository.DeviceRepository
+import com.phew.domain.repository.network.ProfileRepository
 import javax.inject.Inject
 
-class CheckIsMyProfile @Inject constructor(private val repository: DeviceRepository) {
+class CheckIsMyProfile @Inject constructor(private val repository: ProfileRepository) {
     data class Param(val userId: Long, val nickName: String)
 
     suspend operator fun invoke(param: Param): DomainResult<Pair<Boolean, Long>, String> {
-        val myProfile =
-            repository.getProfileInfo(BuildConfig.PROFILE_KEY) ?: return DomainResult.Failure(
-                ERROR_FAIL_JOB
-            )
-        if (myProfile == param.nickName) return DomainResult.Success(Pair(true, param.userId))
-        return DomainResult.Success(Pair(false, param.userId))
+        return when (val request = repository.requestMyProfile()) {
+            is DataResult.Fail -> {
+                DomainResult.Failure(request.message ?: ERROR_FAIL_JOB)
+            }
+
+            is DataResult.Success -> {
+                if (request.data.nickname != param.nickName) {
+                    return DomainResult.Success(Pair(false, param.userId))
+                }
+                if (request.data.userId != param.userId) {
+                    return DomainResult.Success(Pair(false, param.userId))
+                }
+                return DomainResult.Success(Pair(true, request.data.userId))
+            }
+        }
     }
 }
