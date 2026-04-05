959e6a8326fcb744bbeb3dfdb5195d43184fe05b
sonny8569 <sonny132@naver.com>
2026-04-05 22:54:48 +0900

    1.0.5 배포 구글 애드몹 난독화 금지

--- build-logic/src/main/kotlin/convention/ApplicationConvention.kt
+++ build-logic/src/main/kotlin/convention/ApplicationConvention.kt
@@ -33,8 +33,8 @@ class ApplicationConvention : Plugin<Project> {
                 applicationId = "com.phew.sooum"
                 minSdk = 31
                 targetSdk = 36
-                versionCode = 12
-                versionName = "1.0.4"
+                versionCode = 13
+                versionName = "1.0.5"
                 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                 val appLink = propertiesKeys.getProperty("playStore_app_url", "")
                 buildConfigField("String", "PLAY_STORE_LINK", appLink)
