8d608a38d6255f1a433863f01ceefde600e28cb0
sonny8569 <sonny132@naver.com>
2026-04-10 21:59:26 +0900

    1.0.6 배포-내부테스트

--- build-logic/src/main/kotlin/convention/ApplicationConvention.kt
+++ build-logic/src/main/kotlin/convention/ApplicationConvention.kt
@@ -33,8 +33,8 @@ class ApplicationConvention : Plugin<Project> {
                 applicationId = "com.phew.sooum"
                 minSdk = 31
                 targetSdk = 36
-                versionCode = 13
-                versionName = "1.0.5"
+                versionCode = 14
+                versionName = "1.0.6"
                 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                 val appLink = propertiesKeys.getProperty("playStore_app_url", "")
                 buildConfigField("String", "PLAY_STORE_LINK", appLink)
