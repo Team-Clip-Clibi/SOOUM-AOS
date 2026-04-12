d292db6fff7023eaf416f9a329f93d0eefd948ce
sonny8569 <sonny132@naver.com>
2026-04-12 22:48:43 +0900

    1.0.34 배포

--- build-logic/src/main/kotlin/convention/ApplicationConvention.kt
+++ build-logic/src/main/kotlin/convention/ApplicationConvention.kt
@@ -34,7 +34,7 @@ class ApplicationConvention : Plugin<Project> {
                 minSdk = 31
                 targetSdk = 36
                 versionCode = 1
-                versionName = "1.0.33"
+                versionName = "1.0.34"
                 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                 val appLink = propertiesKeys.getProperty("playStore_app_url", "")
                 buildConfigField("String", "PLAY_STORE_LINK", appLink)
