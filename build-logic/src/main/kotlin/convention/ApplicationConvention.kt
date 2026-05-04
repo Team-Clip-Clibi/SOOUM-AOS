5cad52550531b53b4ad36939e6ddd810565f4bbf
sonny8569 <sonny132@naver.com>
2026-05-05 01:43:14 +0900

    SOOUM-513

--- build-logic/src/main/kotlin/convention/ApplicationConvention.kt
+++ build-logic/src/main/kotlin/convention/ApplicationConvention.kt
@@ -79,8 +79,14 @@ class ApplicationConvention : Plugin<Project> {
                     dimension = "environment"
                     applicationIdSuffix = ".dev"
                     versionNameSuffix = "-dev"
+                    manifestPlaceholders["APP_NAME"] = "[D]SOOUM"
+                    manifestPlaceholders["APP_ICON"] = "@mipmap/ic_sooum_logo_debug"
+                    manifestPlaceholders["APP_ROUND_ICON"] = "@mipmap/ic_sooum_logo_debug_round"
                 }
                 create("prod") {
+                    manifestPlaceholders["APP_NAME"] = "@string/app_name"
+                    manifestPlaceholders["APP_ICON"] = "@mipmap/ic_sooum_logo"
+                    manifestPlaceholders["APP_ROUND_ICON"] = "@mipmap/ic_sooum_logo_round"
                     dimension = "environment"
                 }
             }
