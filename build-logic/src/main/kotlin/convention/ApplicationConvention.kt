1a804625487e6424b13a2af2985f8600246069e3
sonny8569 <sonny132@naver.com>
2026-07-28 11:52:38 +0900

    1.0.7 버전 배포

--- build-logic/src/main/kotlin/convention/ApplicationConvention.kt
+++ build-logic/src/main/kotlin/convention/ApplicationConvention.kt
@@ -33,7 +33,7 @@ class ApplicationConvention : Plugin<Project> {
                 applicationId = "com.phew.sooum"
                 minSdk = 31
                 targetSdk = 36
-                versionCode = 21
+                versionCode = 22
                 versionName = "1.0.7"
                 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                 val appLink = propertiesKeys.getProperty("playStore_app_url", "")
